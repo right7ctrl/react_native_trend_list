e583475cf637bb153afd2ea1eb1a17a30e61a824
murat aslan <murataslanx33@gmail.com>
2020-11-17 21:36:01 +0300

    install react-navigation

---
+++ App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/pages/Home';
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
